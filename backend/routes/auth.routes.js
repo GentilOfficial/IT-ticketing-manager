8f012c3585de7f1793ee4e9605a42accedae1df7
Federico Gentili <federicogentili05@gmail.com>
2026-06-21 20:23:03 +0200

    implemented express-validator (#5)

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const authRoutes = express.Router()
 const authController = require('../controllers/auth.controller')
+const { validateRegister, validateLogin } = require('../middleware/validation/auth.validation.middleware')
 
-authRoutes.post('/register', authController.register)
-authRoutes.post('/login', authController.login)
+authRoutes.post('/register', validateRegister, authController.register)
+authRoutes.post('/login', validateLogin, authController.login)
 authRoutes.get('/me', authController.loggedUser)
 
 module.exports = { authRoutes }
