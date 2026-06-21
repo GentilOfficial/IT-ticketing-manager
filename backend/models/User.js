ce7f498ec3ff6d90e1477937bf24bffb5c41111d
Federico Gentili <federicogentili05@gmail.com>
2026-06-21 15:26:42 +0200

    implemented dev-163 User model

---
+++ backend/models/User.js
@@ -0,0 +1,54 @@
+const { Schema, model } = require('mongoose')
+const { hash, compare } = require('bcrypt')
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 60,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    role: {
+      type: String,
+      enum: ['user', 'admin'],
+      default: 'user',
+    },
+    oauth: {
+      provider: {
+        type: String,
+        enum: ['local', 'google'],
+        default: 'local',
+      },
+      id: {
+        type: String,
+        select: false,
+      },
+    },
+  },
+  { timestamps: true, strict: true },
+)
+
+userSchema.pre('save', async function () {
+  if (!this.isModified('password') || !this.password) return
+  this.password = await hash(this.password, 12)
+})
+
+userSchema.methods.comparePassword = function (password) {
+  return compare(password, this.password)
+}
+
+module.exports = model('user', userSchema, 'users')
