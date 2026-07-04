70b92c97d3c1b632ae5f01bedcc45242c9042574
Federico Gentili <federicogentili05@gmail.com>
2026-07-04 20:54:17 +0200

    added MongoStore for session management with MongoDB

--- backend/config/session.js
+++ backend/config/session.js
@@ -1,4 +1,5 @@
 const session = require('express-session')
+const MongoStore = require('connect-mongo').default
 
 const initSessionCookies = (server) => {
   const isProduction = process.env.NODE_ENV === 'production'
@@ -11,6 +12,9 @@ const initSessionCookies = (server) => {
       resave: false,
       saveUninitialized: false,
       rolling: true,
+      store: MongoStore.create({
+        mongoUrl: process.env.MONGODB_CONNECTION_STRING,
+      }),
       cookie: {
         httpOnly: true,
         secure: isProduction,
