0eb632b2d24f975d6190e12918dcf91cbf8df994
Federico Gentili <federicogentili05@gmail.com>
2026-07-05 13:31:43 +0200

    added user badge for logged-in user in user list

--- frontend/src/components/users/UsersListItem.jsx
+++ frontend/src/components/users/UsersListItem.jsx
@@ -1,12 +1,15 @@
+import { Badge } from '@/components/ui/badge'
 import { Button } from '@/components/ui/button'
 import { Separator } from '@/components/ui/separator'
 import AuthProviderBadge from '@/components/users/AuthProviderBadge'
 import EditUserDialog from '@/components/users/EditUserDialog'
 import UserRoleBadge from '@/components/users/UserRoleBadge'
-import { Pencil } from 'lucide-react'
+import { useAuth } from '@/context/AuthContext'
+import { Pencil, UserStar } from 'lucide-react'
 import moment from 'moment'
 
 const UsersListItem = ({ user, isLast = false, onUserUpdated }) => {
+  const { user: authUser } = useAuth()
   const createdAt = moment(user.createdAt).format('MMM YYYY')
   return (
     <>
@@ -14,7 +17,18 @@ const UsersListItem = ({ user, isLast = false, onUserUpdated }) => {
         <div className="flex items-start sm:items-center gap-3 min-w-0 w-full sm:w-auto">
           <div className="min-w-0 flex-1">
             <div className="flex flex-col sm:flex-row sm:items-center gap-0.5 sm:gap-2">
-              <span className="text-sm font-medium leading-tight truncate">{user.name}</span>
+              <div className="flex items-center gap-1">
+                <span className="text-sm font-medium leading-tight truncate">{user.name}</span>
+                {user._id === authUser._id && (
+                  <>
+                    <Separator orientation="vertical" />
+                    <Badge variant="outline">
+                      <UserStar />
+                      <span>you</span>
+                    </Badge>
+                  </>
+                )}
+              </div>
               <span className="text-sm text-muted-foreground truncate">{user.email}</span>
             </div>
             <div className="mt-1.5 flex items-center gap-2 flex-wrap">
