b73c4b731f237500c15a14f3688459bb93874a33
Federico Gentili <federicogentili05@gmail.com>
2026-07-18 13:00:47 +0200

    Api docs (#30)
    * added api docs link on AuthLayout
    
    * added api docs link on AppSidebar

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -1,4 +1,5 @@
-import { Headset } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import { Headset, Scroll } from 'lucide-react'
 import { Link } from 'react-router'
 
 const AuthLayout = ({ children }) => {
@@ -14,6 +15,14 @@ const AuthLayout = ({ children }) => {
           </Link>
         </div>
         <div className="flex flex-1 items-center justify-center">{children}</div>
+        <div className="flex justify-center gap-2 md:justify-end">
+          <Button asChild variant="outline" className="text-muted-foreground rounded-full">
+            <a href={`${import.meta.env.VITE_API_SERVER}/docs`} target="_blank">
+              <Scroll className="size-6 p-1" />
+              <span>API Documentation</span>
+            </a>
+          </Button>
+        </div>
       </div>
     </div>
   )
