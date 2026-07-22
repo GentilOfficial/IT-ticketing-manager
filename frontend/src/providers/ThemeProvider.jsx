cfddf24291736a07dffcf1dcd3bec65ca757e54d
Federico Gentili <federicogentili05@gmail.com>
2026-07-22 11:44:10 +0200

    added theme provider

---
+++ frontend/src/providers/ThemeProvider.jsx
@@ -0,0 +1,11 @@
+import { ThemeProvider as NextThemesProvider } from 'next-themes'
+
+const ThemeProvider = ({ children }) => {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      {children}
+    </NextThemesProvider>
+  )
+}
+
+export default ThemeProvider
