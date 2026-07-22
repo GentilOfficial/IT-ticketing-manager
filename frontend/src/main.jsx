5e14306f43903b961f000c9d93a9e5418a7dbee7
Federico Gentili <federicogentili05@gmail.com>
2026-07-22 11:44:38 +0200

    implemented theme provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import App from '@/App.jsx'
 import '@/index.css'
+import ThemeProvider from '@/providers/ThemeProvider'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
